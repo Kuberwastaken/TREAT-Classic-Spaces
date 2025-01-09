81cd359efd4f820d20f527e1a5edb6c03f09e2a3
Kuberwastaken <Kuberhob@gmail.com>
2025-01-09 22:10:54 +0530

    Changed Parameters

--- model/analyzer.py
+++ model/analyzer.py
@@ -172,9 +172,9 @@ class ContentAnalyzer:
                 with torch.no_grad():
                     outputs = self.model.generate(
                         **inputs,
-                        max_new_tokens=3,
+                        max_new_tokens=5,
                         do_sample=True,
-                        temperature=0.7,
+                        temperature=0.3,
                         top_p=0.8,
                         pad_token_id=self.tokenizer.eos_token_id
                     )
