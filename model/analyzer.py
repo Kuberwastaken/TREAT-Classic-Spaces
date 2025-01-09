c689a9384cf992e239b735a0834740097d27c097
Kuberwastaken <Kuberhob@gmail.com>
2025-01-09 21:40:12 +0530

    I have no clue why this works, but this works

--- model/analyzer.py
+++ model/analyzer.py
@@ -172,10 +172,10 @@ class ContentAnalyzer:
                 with torch.no_grad():
                     outputs = self.model.generate(
                         **inputs,
-                        max_new_tokens=10,
+                        max_new_tokens=3,
                         do_sample=True,
-                        temperature=0.3,
-                        top_p=0.9,
+                        temperature=0.7,
+                        top_p=0.8,
                         pad_token_id=self.tokenizer.eos_token_id
                     )
 
